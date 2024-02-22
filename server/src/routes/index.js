23fadc10498cc030629c3675b0ad23f64d8282bc
RNDOSKO <Ricardo.dosko@gmail.com>
2024-02-22 13:18:01 -0300

    Dependency install & DB changes

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,5 +1,12 @@
 const { Router } = require("express");
+//importo los routers 
+const routerCountry = require('./routerCountry')
+const routerActivity = require('./routerActivity')
 
 const router = Router();
+
+//Configuro los routers
+router.use('/countries', routerCountry)
+router.use('/activities', routerActivity)
 
 module.exports = router;
